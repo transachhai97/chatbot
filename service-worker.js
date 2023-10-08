2d9f916240c5c7a8a0e43c71596d65e81b51aca1
haits <haits@haits>
2023-10-08 17:07:40 +0700

    pwa

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/service-worker.js')
+        .then((registration) => {
+            console.log('Service Worker registered successfully:', registration);
+        })
+        .catch((error) => {
+            console.log('Service Worker registration failed:', error);
+        });
+}
