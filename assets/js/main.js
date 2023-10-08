8c26d6a9b92066e0e16e3a177d49052615384ccf
haits <haits@haits>
2023-10-08 09:07:31 +0700

    list

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
+function resetPage() {
+    localStorage.clear();
+    location.reload();
+}
+
+$(document).ready(function () {
+    console.log("------>bidv: convertBotList: ", convertBotList);
+    convertBotList.forEach(renderBotList);
+});
+
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
 
@@ -10,3 +20,59 @@ $(function () {
         }
     });
 })
+
+function renderBotList(item, index) {
+    console.log("------>bidv: item: ", index, item);
+
+    let elmHtml =
+        '<div class="d-flex flex-column p-2 chat-item" data-toggle="tooltip" data-placement="top" title="' + item.botName + '" id="bot-' + index + '" onclick="clickBotItem(`' + index + '`)">' +
+        '   <div class="py-2 d-flex align-items-center">' +
+        '       <img class="me-2 rounded rounded-circle chat-item--logo" src="' + item.logo + '">' +
+        '       <h6 class="flex-fill my-2 text-truncate">' + item.botName + '</h6>' +
+        '   </div>' +
+        '</div>';
+
+    $('#chat-list').append(elmHtml);
+}
+
+function clickBotItem(index) {
+    let item = convertBotList[index];
+
+    $('#chat-content').hide();
+    $('.chat-item').removeClass('active');
+
+    $('#bot-' + index).addClass('active');
+
+    let url = '';
+    switch (item.type) {
+        case 'private':
+            url = 'https://bidv-livechat.fpt.ai/v36/src/index.html?botcode=' + item.botId;
+            break;
+
+        case 'public':
+            url = 'https://livechat.fpt.ai/v36/src/index.html?botcode=' + item.botId + '&' + new URLSearchParams(jsonConfig).toString();
+            break;
+
+        default:
+            alert('Lỗi môi trường');
+            return;
+    }
+
+    $('#chat-name').text(item.botName);
+    $('#chat-icon').attr('src', item.logo);
+    $('#chat-box').attr('src', url);
+
+    console.log("------>bidv: urlIframe: ", url);
+
+    $('#chat-content').show(500);
+
+    let arrRecent = JSON.parse(localStorage.getItem(keyRecent)) || [];
+
+    arrRecent.unshift(index);
+
+    arrRecent = [...new Set(arrRecent)];
+
+    localStorage.setItem(keyCurrent, index);
+
+    localStorage.setItem(keyRecent, JSON.stringify(arrRecent.slice(0, recentLength)));
+}
---
+++ assets/js/variable.js
@@ -0,0 +1,119 @@
+// Số lượng bot hiển thị gần đây
+const recentLength = 3;
+
+const keyRecent = 'arrRecent';
+const keyCurrent = 'currentId';
+
+// Danh sách bot
+const botList = [
+    {
+        type: 'private',
+        botId: '8658788cd6ffa8a7c4f0065298388f52',
+        botName: '[TTT] BOT NỘI BỘ BIDV',
+        logo: '',
+    },
+    {
+        type: 'private',
+        botId: '8883508d6f0b9256e6c347f49e7728f9',
+        botName: '[UAT] BIDV',
+        logo: '',
+    },
+    {
+        type: 'public',
+        botId: '8f7e0d766ab7df1188a36e71bdcc9faf',
+        botName: '[UAT-C] BIDV -ver2',
+        logo: '',
+    },
+    {
+        type: 'private',
+        botId: '1',
+        botName: '1',
+        logo: '',
+    },
+    {
+        type: 'public',
+        botId: '2',
+        botName: '2',
+        logo: '',
+    },
+    {
+        type: 'public',
+        botId: '3',
+        botName: '3',
+        logo: '',
+    },
+    {
+        type: 'public',
+        botId: '4',
+        botName: '4',
+        logo: '',
+    },
+    {
+        type: 'public',
+        botId: '5',
+        botName: '5',
+        logo: '',
+    },
+    {
+        type: 'public',
+        botId: '6',
+        botName: '6',
+        logo: '',
+    },
+    {
+        type: 'public',
+        botId: '7',
+        botName: '7',
+        logo: '',
+    },
+];
+
+const styleConfig = {
+    // header
+    headerBackground: 'linear-gradient(86.7deg, #006B68FF 0.85%, #005452FF 98.94%)',
+    headerTextColor: '#ffffffff',
+    headerLogoEnable: true,
+    headerLogoLink: 'https://chatbot-tools.fpt.ai/livechat-builder/img/theme/bank/logo.svg',
+    headerText: 'Live support',
+    // main
+    primaryColor: '#005452FF',
+    secondaryColor: '#D8D8D8FF',
+    primaryTextColor: '#ffffffff',
+    secondaryTextColor: '#1F1F1FFF',
+    buttonColor: '#FFC62FFF',
+    buttonTextColor: '#ffffffff',
+    bodyBackgroundEnable: true,
+    bodyBackgroundLink: 'https://chatbot-tools.fpt.ai/livechat-builder/img/theme/bank/body.png',
+    avatarBot: 'https://bidv-livechat.fpt.ai/v36/src/imgBIDV/logo-bidv-full.png',
+    sendMessagePlaceholder: 'Enter your message here',
+    // float button
+    floatButtonLogo: 'https://chatbot-tools.fpt.ai/livechat-builder/img/theme/bank/coin.svg',
+    floatButtonTooltip: 'Can I help you?',
+    floatButtonTooltipEnable: false,
+    // start screen
+    customerLogo: 'https://bidv-livechat.fpt.ai/v36/src/imgBIDV/logo-bidv-full.png',
+    customerWelcomeText: 'Nhập email của bạn (vd: xxx@bidv.com.vn)',
+    customerButtonText: 'Bắt đầu',
+    prefixEnable: false,
+    prefixType: 'radio',
+    prefixOptions: ["Anh", "Chị"],
+    prefixPlaceholder: 'Danh xưng',
+    // custom css
+    css: ''
+};
+
+const jsonConfig = {
+    "styles": JSON.stringify(styleConfig),
+};
+
+const botListLength = botList.length;
+const convertBotList = [];
+
+for (let i = 0; i < botListLength; i++) {
+    let item = botList[i];
+    let newItem = {...item};
+    if (!item?.logo) {
+        newItem.logo = './assets/images/icon.png';
+    }
+    convertBotList.push(newItem);
+}
